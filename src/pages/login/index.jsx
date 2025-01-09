6e02b20c09bad0d8fc9207c8c07790b3adf8ef02
rakafebriansy <kerenr445@gmail.com>
2025-01-09 18:21:00 +0700

    fix: sweetalert

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -22,6 +22,7 @@ const LoginPage = ({  }) => {
                 icon: "success",
                 title: "Login Berhasil",
                 text: "Selamat datang!",
+                timer: 5000
             });
             setTimeout(() => {
                 navigate('/home');
@@ -31,6 +32,7 @@ const LoginPage = ({  }) => {
                 icon: "error",
                 title: "Login Gagal",
                 text: "Username atau password salah!",
+                timer: 5000
             });
         }
     };
@@ -70,10 +72,10 @@ const LoginPage = ({  }) => {
                     </div>
 
                     <div className="mt-5">
-                        <form>
+                        <div>
                             <div className="grid gap-y-4">
                             <div>
-                                <label for="username" className="block text-sm mb-2 dark:text-white">Username</label>
+                                <label htmlFor="username" className="block text-sm mb-2 dark:text-white">Username</label>
                                 <div className="relative">
                                     <input type="text" onChange={(e) => setUsername(e.target.value)} id="username" name="username" className="py-3 border shadow-sm px-4 block w-full border-gray-300 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="email-error"/>
                                 </div>
@@ -83,7 +85,7 @@ const LoginPage = ({  }) => {
                             </div>
                             <div>
                                 <div className="flex justify-between items-center">
-                                <label for="password" className="block text-sm mb-2 dark:text-white">Password</label>
+                                <label htmlFor="password" className="block text-sm mb-2 dark:text-white">Password</label>
                                 {/* <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="../examples/html/recover-account.html">Forgot password?</a> */}
                                 </div>
                                 <div className="relative">
@@ -91,14 +93,14 @@ const LoginPage = ({  }) => {
                                     {isHidden ? (
                                         <div className="absolute top-1/2 -translate-y-1/2 right-1 pe-3">
                                             <svg onClick={() => setIsHidden(!isHidden)} className="w-5 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                             </svg>
                                         </div>
                                     ) : (
                                         <div className="absolute top-1/2 -translate-y-1/2 right-1 pe-3">
                                             <svg onClick={() => setIsHidden(!isHidden)} className="w-5 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                             </svg>
                                         </div>
                                     )}
@@ -112,12 +114,12 @@ const LoginPage = ({  }) => {
                                 <input id="remember-me" name="remember-me" type="checkbox" className="shrink-0 mt-0.5 border-gray-300 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"/>
                                 </div>
                                 <div className="ms-3">
-                                <label for="remember-me" className="text-sm dark:text-white">Remember me</label>
+                                <label htmlFor="remember-me" className="text-sm dark:text-white">Remember me</label>
                                 </div>
                             </div> */}
                             <button onClick={login} disabled={(errorBags.password.length > 0 || errorBags.password.length > 0)} className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Sign in</button>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
