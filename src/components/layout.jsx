3f45ddfc0cf209a3634ecc80c6f2da61fcc9a7ab
rakafebriansy <kerenr445@gmail.com>
2025-01-10 07:09:15 +0700

    feat: logout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,11 +1,24 @@
-import { Link, useLocation } from "react-router-dom"
-import { getCookie } from "../helper/cookie";
-import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { getCookie, removeCookie } from "../helper/cookie";
+import { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
     
     const location = useLocation();
+    const navigate = useNavigate();
     const [visible, setVisible] = useState(false);
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
+    
+    const logout = () => {
+        removeCookie('authorized');
+        navigate('/login');
+    }
+
+    useEffect(() => {
+        setUsername(getCookie('authorized'));
+        setFullName(getCookie('fullName'));
+    },[]);
 
     return (
     <main className="relative">
@@ -18,25 +31,31 @@ const Layout = ({ children }) => {
                     
                 </a> */}
             </ul>
-            <div className="">
+            <div className="flex gap-3 items-center">
+                <div className="flex flex-col justify-start text-xs">
+                        <p className="font-bold">{username}</p>
+                        <p>{fullName}</p>
+                </div>
                 <div className="relative inline-flex flex-col">
                     <button onClick={() => setVisible(!visible)} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
                         Menu
                         <svg className={`size-4 ${visible ? "rotate-180" : ""}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
                     </button>
-
-                    <div className={`transition-[opacity,margin] absolute top-full w-32 -translatey-1/2 duration ${visible ? "opacity-100 mt-4" : "opacity-0 mt-0"}
-                        bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full`} role="menu" aria-orientation="vertical">
-                        <div className="p-1 space-y-0.5">
-                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                                Profile
-                            </a>
-                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                                Dark Mode
-                            </a>
-                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                                Logout
-                            </a>
+                    
+                    <div className="flex gap-3">
+                        <div className={`transition-[opacity,margin] absolute top-full w-32 -translatey-1/2 duration ${visible ? "opacity-100 mt-4" : "opacity-0 mt-0"}
+                            bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full`} role="menu" aria-orientation="vertical">
+                            <div className="p-1 space-y-0.5">
+                                <Link to={'/edit-profile'} className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
+                                    Profile
+                                </Link>
+                                <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
+                                    Dark Mode
+                                </a>
+                                <button onClick={logout} className="flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
+                                    Logout
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/helper/cookie.js
+++ src/helper/cookie.js
@@ -17,6 +17,6 @@ export function getCookie(name) {
     return null;
 }
 
-export function deleteCookie(name) {
+export function removeCookie(name) {
     document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;Secure;SameSite=Strict";
 }
